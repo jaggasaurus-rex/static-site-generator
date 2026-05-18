a1bfd033f31b27d58af425f130c46165102f2299
jaggasaurus-rex <jaggers.tyler@gmail.com>
2026-05-17 17:33:43 -0700

    can now handle blocks of raw text

--- src/block_handler.py
+++ src/block_handler.py
@@ -1,5 +1,9 @@
 from enum import Enum
 
+from htmlnode import HTMLNode, ParentNode, LeafNode
+from textnode import text_node_to_html_node, TextNode, TextType
+from rawmarkdown import text_to_textnodes
+
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
     final = []
@@ -47,3 +51,105 @@ def block_to_block_type(block):
         return BlockType.OLIST
     return BlockType.PARAGRAPH
     
+def markdown_to_html_node(markdown):
+    blocks = markdown_to_blocks(markdown)
+    outter_children = []
+    for block in blocks:
+        block_type = block_to_block_type(block)
+        if block_type == BlockType.CODE:
+            inner_text = block[4:-3]
+            code_leaf = LeafNode("code", inner_text)
+            pre_parent = ParentNode("pre",[code_leaf])
+            outter_parent = pre_parent
+        else:
+            outter_parent = type_to_tag(block, block_type)
+        outter_children.append(outter_parent)
+
+
+    final_node = ParentNode("div", outter_children)
+    return final_node
+
+
+def type_to_tag(text, type):
+    if type == BlockType.PARAGRAPH:
+        text_value = paragraph_to_html(text)
+        text_node = text_to_textnodes(text_value)
+        children = text_to_children(text_node)
+        outter_parent = ParentNode("p", children)
+        return outter_parent
+    if type == BlockType.QUOTE:
+        split_text = text.split("\n")
+        text_value = ""
+        for item in split_text:
+            text_value += f"{item[2:]} "
+        text_value = text_value.strip()
+        text_node = text_to_textnodes(text_value)
+        children = text_to_children(text_node)
+        outter_parent = ParentNode("blockquote", children)
+        return outter_parent
+    if type == BlockType.ULIST:
+        children = ulist_to_html(text)
+        outter_parent = ParentNode("ul", children)
+        return outter_parent
+    if type == BlockType.OLIST:
+        children = olist_to_html(text)
+        outter_parent = ParentNode("ol", children)
+        return outter_parent
+    if type == BlockType.HEADING:
+        tag, text_value = heading_to_html(text)
+        text_node = text_to_textnodes(text_value)
+        children = text_to_children(text_node)
+        outter_parent = ParentNode(tag, children)
+        return outter_parent
+    raise ValueError("Error: Not a valid block type")
+    
+def heading_to_html(text):
+    if text.startswith("# "):
+        level = 1
+    elif text.startswith("## "):
+        level = 2
+    elif text.startswith("### "):
+        level = 3
+    elif text.startswith("#### "):
+        level = 4
+    elif text.startswith("##### "):
+        level = 5
+    elif text.startswith("###### "):
+        level = 6
+    else:
+        raise ValueError("Error: Not a valid block type")
+    tag = f"h{level}"
+    value = text[level + 1:]
+    return tag, value
+
+def paragraph_to_html(text):
+    new_text = text.replace("\n", " ")
+    return new_text
+
+def ulist_to_html(text):
+    ulist_items = text.split("\n")
+    full = []
+    for item in ulist_items:
+        text_node = text_to_textnodes(item[2:])
+        children = text_to_children(text_node)
+        outter_parent = ParentNode("li", children)
+        full.append(outter_parent)
+    return full
+
+def olist_to_html(text):
+    olist_items = text.split("\n")
+    full = []
+    for item in olist_items:
+        inner = item.split(". ", 1)[1]
+        text_node = text_to_textnodes(inner)
+        children = text_to_children(text_node)
+        outter_parent = ParentNode("li", children)
+        full.append(outter_parent)
+    return full
+
+def text_to_children(text):
+    children = []
+    for node in text:
+        leaf_node = text_node_to_html_node(node)
+        children.append(leaf_node)
+    return children
--- src/easy_tester.py
+++ src/easy_tester.py
@@ -1,33 +1,68 @@
 from htmlnode import HTMLNode, LeafNode, ParentNode
 from textnode import text_node_to_html_node, TextNode, TextType
 from rawmarkdown import split_nodes_delimiter, extract_markdown_images, split_nodes_image, split_nodes_link, text_to_textnodes
-from block_handler import markdown_to_blocks, block_to_block_type
+from block_handler import markdown_to_blocks, block_to_block_type, markdown_to_html_node
 
 def easytestcase():
     md = """
-#### This is **bolded** paragraph
+This is **bolded** paragraph
+text in a p
 
+> fuck bitches
 
+# tag here
 
-''' this is code'''
+## fucking head 2 bitch
 
 This is another paragraph with _italic_ text and `code` here
-This is the same paragraph on a new line
 
-
->This is another paragraph with _italic_ text and `code` here
->This is the same paragraph on a new line
-
-- This is a list
-- with items
-
-1. This is a list
-2. with items
-3. and more
-4. items
 """
-    blocks = markdown_to_blocks(md)
-    blocktype = block_to_block_type(blocks[0])
-    print(blocktype)
 
-easytestcase()
+    node = markdown_to_html_node(md)
+    html = node.to_html()
+    print(html)
+
+def easytestcase2():
+    md = """
+```
+This is text that _should_ remain
+the **same** even with inline stuff
+```
+"""
+
+    node = markdown_to_html_node(md)
+    html = node.to_html()
+    print(html)
+
+def easytestcase3():
+    md = """
+This is **bolded** paragraph
+text in a p
+
+> fuck bitches
+
+> get hoes
+> is what
+> I've been saying
+
+- this is
+- a fucking
+- list bitch
+
+1. this is
+2. an ordered
+3. list bitch
+
+# tag here
+
+## another tag here
+
+This is another paragraph with _italic_ text and `code` here
+
+"""
+
+    node = markdown_to_html_node(md)
+    html = node.to_html()
+    print(html)
+
+easytestcase2()
