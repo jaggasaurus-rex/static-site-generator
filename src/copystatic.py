ddd88befec2052eb2abd83335490447efbc4fcfb
jaggasaurus-rex <jaggers.tyler@gmail.com>
2026-05-20 11:57:09 -0700

    actual site generation complete

--- src/copystatic.py
+++ src/copystatic.py
@@ -1,5 +1,6 @@
 import shutil
 import os
+from block_handler import markdown_to_html_node
 
 def source_to_destination():
     source_path = "static"
@@ -24,3 +25,30 @@ def new_directory_generator(source_path,dest_path):
             if os.path.isdir(new_source_path):
                 os.mkdir(new_dest_path)
             new_directory_generator(new_source_path,new_dest_path)
+
+def extract_title(markdown):
+    split = markdown.split("\n")
+    title = ""
+    for line in split:
+        if line.startswith("# "):
+            title = line[2:]
+            break
+    if title == "":
+        raise Exception("No title line")
+    return title
+
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    with open(from_path) as f:
+        article = f.read()
+    title = extract_title(article)
+    with open(template_path) as f:
+        template = f.read()
+    converted = markdown_to_html_node(article)
+    content = converted.to_html()
+    updated_title = template.replace("{{ Title }}", title)
+    updated_content = updated_title.replace("{{ Content }}", content)
+    file_dir = os.path.dirname(dest_path)
+    os.makedirs(file_dir, exist_ok=True)
+    with open(dest_path, "w") as f:
+        f.write(updated_content)
--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 from textnode import TextNode, TextType
-from copystatic import source_to_destination
+from copystatic import source_to_destination, extract_title, generate_page
 
 def main():
     source_to_destination()
-    
+    generate_page("content/index.md","template.html","public/index.html")
+
 main()
